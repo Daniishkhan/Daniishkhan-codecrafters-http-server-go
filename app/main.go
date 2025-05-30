243e7d634bff2c55befd5a5ef13e29dee73d0038
Daniishkhan <danishafzalkhan@gmail.com>
2025-05-31 04:28:36 +0500

    added error handling

--- app/main.go
+++ app/main.go
@@ -3,23 +3,10 @@ package main
 import (
 	"fmt"
 	"net"
-)
-
-func handleConnection(conn net.Conn) {
-	// defer conn.Close() // Always clean up when done
-
-	// Your existing parsing logic goes here
-	// Read request, parse headers, send response
-
-	fmt.Printf("🏁 Connection handled\n")
-}
-apackage main
-import (
-	"fmt"
-	"net"
 	"os"
 	"strings"
 )
+
 func main() {
 	l, err := net.Listen("tcp", "0.0.0.0:4221")
 	if err != nil {
@@ -37,25 +24,58 @@ func main() {
 		go handleRequest(conn)
 	}
 }
-func main() {
-	listener, err := net.Listen("tcp", ":4221")
+func handleRequest(conn net.Conn) {
+	data := make([]byte, 4096)
+	_, err := conn.Read(data)
 	if err != nil {
-		panic(err)
+		fmt.Printf("Error reading: %s\n", err)
+		os.Exit(1)
 	}
-	defer listener.Close()
-
-	fmt.Printf("🚀 Server listening on :4221\n")
-
-	for {
-		conn, err := listener.Accept()
-		if err != nil {
-			fmt.Printf("❌ Accept error: %v\n", err)
-			continue
+	lines := strings.Split(string(data), "\r\n")
+	firstLineParts := strings.Split(lines[0], " ")
+	method := firstLineParts[0]
+	path := firstLineParts[1]
+	userAgent := ""
+	for _, line := range lines {
+		if strings.HasPrefix(line, "User-Agent: ") {
+			userAgent = strings.TrimPrefix(line, "User-Agent: ")
+			userAgent = strings.TrimSpace(userAgent)
+			break
 		}
-
-		fmt.Printf("📞 New connection accepted\n")
-
-		// The magic line - handle each connection concurrently
-		go handleConnection(conn)
 	}
+	if method != "GET" {
+		fmt.Println("Only GET is supported")
+		os.Exit(1)
+	}
+	if path == "/" {
+		okRes := createHttpResp(200, []byte(""), "text/plain")
+		conn.Write([]byte(okRes))
+	} else if strings.HasPrefix(path, "/echo/") {
+		suffix := strings.TrimPrefix(path, "/echo/")
+		okRes := createHttpResp(200, []byte(suffix), "text/plain")
+		conn.Write([]byte(okRes))
+	} else if path == "/user-agent" {
+		okRes := createHttpResp(200, []byte(userAgent), "text/plain")
+		conn.Write([]byte(okRes))
+	} else {
+		notFoundRes := createHttpResp(404, []byte(""), "text/plain")
+		conn.Write([]byte(notFoundRes))
+	}
+	conn.Close()
+}
+func createHttpResp(status int, body []byte, contentType string) string {
+	resp := "HTTP/1.1 "
+	switch status {
+	case 200:
+		resp += fmt.Sprintf("200 OK\r\n")
+	case 404:
+		resp += fmt.Sprintf("404 Not Found\r\n")
+	}
+	if len(body) > 0 {
+		resp += fmt.Sprintf("Content-Type: %s\r\n", contentType)
+		resp += fmt.Sprintf("Content-Length: %d\r\n\r\n%s", len(body), string(body))
+	} else {
+		resp += fmt.Sprintf("Content-Length: 0\r\n\r\n")
+	}
+	return resp
 }
