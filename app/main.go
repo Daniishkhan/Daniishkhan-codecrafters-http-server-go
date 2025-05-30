d97f190477fa65cf948cfd5b5c9072a15691bf72
Daniishkhan <danishafzalkhan@gmail.com>
2025-05-31 03:44:12 +0500

    added error handling

--- app/main.go
+++ app/main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"net"
 	"os"
+	"strings"
 	"time"
 )
 
@@ -33,11 +35,40 @@ func main() {
 	time.Sleep(100 * time.Millisecond)
 	defer conn.Close()
 
-	response := "HTTP/1.1 200 OK\r\n\r\n"
-	_, err = conn.Write([]byte(response))
+	reader := bufio.NewReader(conn)
+
+	requestLine, _, err := reader.ReadLine()
 	if err != nil {
-		fmt.Println("Error writing response:", err.Error())
-		os.Exit(1)
+		fmt.Print("error reading request line: ", err.Error())
+		return
+	}
+	parts := strings.Split(string(requestLine), " ")
+	if len(parts) >= 3 {
+		method := parts[0]      // "GET"
+		url := parts[1]         // "/hello"
+		httpVersion := parts[2] // "HTTP/1.1"
+
+		fmt.Printf("Method: %s\n", method)
+		fmt.Printf("URL: %s\n", url)
+		fmt.Printf("HTTP Version: %s\n", httpVersion)
+
+		// Check if we have a valid URL (not empty or just a space)
+		if url != "" && url != " " {
+			response := "HTTP/1.1 200 OK\r\n\r\n"
+			_, err = conn.Write([]byte(response))
+			if err != nil {
+				fmt.Println("Error writing response:", err.Error())
+				os.Exit(1)
+			}
+		} else {
+			// Send error response for invalid URLs
+			response2 := "HTTP/1.1 400 Bad Request\r\n\r\n"
+			_, err = conn.Write([]byte(response2))
+			if err != nil {
+				fmt.Println("Error writing error response:", err.Error())
+				os.Exit(1)
+			}
+		}
 	}
 
 }
